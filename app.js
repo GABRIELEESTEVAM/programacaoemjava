05f99272b2c737281cf50c8f7df660febec94d31
GABRIELEESTEVAM <160232805+GABRIELEESTEVAM@users.noreply.github.com>
2024-07-26 12:04:49 -0300

    ij

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ let chute = prompt('Escolha um número entre 1 e 100');
 if (chute == numeroSecreto) {
 alert('Isso ai! Você descobriu o número secreto (99)');
 } else {
-alert('Você fodeu! :(')
+alert('Se fodeu! :(')
 }
